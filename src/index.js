4d9bf864308a930d97daf4a35769af47f4cb4ff2
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-11-26 19:02:47 -0500

    Initialize new npm project with eslint and prettier

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import css
+import './static/reset.css';
+import './static/style.css';
+
+// import modules (events optional)
+// import * as Events from './assets/events';
+
+console.log('Yo world!');
