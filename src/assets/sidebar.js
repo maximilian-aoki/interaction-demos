fe55432aff597c63341075a26e7492c8ff1a8336
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-11-27 12:20:06 -0500

    fully functioning sidebar with links

--- src/assets/sidebar.js
+++ src/assets/sidebar.js
@@ -1,15 +1,21 @@
 const showSidebar = (function () {
   // dom caching
   const sidebar = document.querySelector('.sidebar');
-  const trigger = document.querySelector('html');
+  const activateButton = document.querySelector('.sidebar-activate>button');
+  const deactivateButton = document.querySelector('.sidebar-header>button');
 
   // methods
   function show() {
     sidebar.classList.toggle('active');
   }
 
+  function hide() {
+    sidebar.classList.toggle('active');
+  }
+
   // event binding
-  trigger.addEventListener('click', show);
+  activateButton.addEventListener('click', show);
+  deactivateButton.addEventListener('click', hide);
 })();
 
 export default showSidebar;
